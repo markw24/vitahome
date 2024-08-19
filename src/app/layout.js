495e01f201e4d10afd84f6870ccaeded06fa0f70
mominalvi <mominalvi30@gmail.com>
2024-08-19 15:19:10 -0400

    Add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,19 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css';
+import Head from 'next/head';
+
+export const metadata = {
+  title: 'Vita Home',
+  description: 'Your Site Description',
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
